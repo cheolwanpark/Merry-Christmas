9ee2a42720cd3de7b7e078402d1d65164b8b0a5d
Cheolwan-Park <cheolwan.park552@gmail.com>
2017-12-12 08:08:11 +0900

    fix bug

--- Merry_Christmas/christmas_code.cpp
+++ Merry_Christmas/christmas_code.cpp
@@ -48,7 +48,7 @@ for(int     i=0     ;i<100; ++i) {  if(s[i*2+1] > 0) {p[(s[i*2+1]-1
  1234L+             fc*r;sp = 100 == sp ? 0 : sp;for(int i=0; i<10;
 i++) {r=(         ((rs = rs * 214013L + 2531011L) >> 16) & 0x7fff);
 s [ sp*2]=i     *10 + r%8 + 1;s[sp*2+1] = 1;++sp;}sf = 0;} else {sf
-= 1;} system( "clear");puts(p); this_thread :: sleep_for ( chrono::
+= 1;} system( "cls" ); puts(p); this_thread :: sleep_for ( chrono::
 milliseconds(1000));}}int main(){go();gm();thread ut(u);thread mt(m)
-;ut.join();mt.join();}// Merry Christmas~~      by Cheolwan-Park
+;ut.join();mt.join();}                          // Merry Christmas~~
 
